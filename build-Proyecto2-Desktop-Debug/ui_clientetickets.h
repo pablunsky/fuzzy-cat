4134b285de0b379647cb9211c1a919925e17fafe
pablunsky <hpablo677@gmail.com>
2018-04-25 00:30:05 -0600

    Interfaz terminada

--- build-Proyecto2-Desktop-Debug/ui_clientetickets.h
+++ build-Proyecto2-Desktop-Debug/ui_clientetickets.h
@@ -13,8 +13,10 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
@@ -40,7 +42,35 @@ public:
     QWidget *page_2;
     QPushButton *pushButton_3;
     QPushButton *pushButton_4;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_7;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_9;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLabel *label_10;
+    QGroupBox *groupBox;
+    QLabel *label_19;
+    QLabel *label_16;
+    QLabel *label_18;
+    QLabel *label_14;
+    QLabel *label_17;
+    QLabel *label_15;
+    QLabel *label_13;
+    QLabel *label_12;
+    QLabel *label_20;
     QWidget *page_3;
+    QLabel *label_11;
+    QLabel *label_21;
+    QLabel *label_22;
+    QPushButton *pushButton_10;
+    QLineEdit *lineEdit;
+    QPushButton *pushButton_11;
+    QLabel *label_23;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -81,14 +111,14 @@ public:
         label_2->setFont(font1);
         label_3 = new QLabel(page);
         label_3->setObjectName(QStringLiteral("label_3"));
-        label_3->setGeometry(QRect(20, 200, 411, 111));
+        label_3->setGeometry(QRect(20, 280, 411, 111));
         QFont font2;
         font2.setFamily(QStringLiteral("Nimbus Sans L"));
         font2.setPointSize(14);
         label_3->setFont(font2);
         pushButton_5 = new QPushButton(page);
         pushButton_5->setObjectName(QStringLiteral("pushButton_5"));
-        pushButton_5->setGeometry(QRect(170, 330, 131, 51));
+        pushButton_5->setGeometry(QRect(170, 210, 131, 51));
         stackedWidget->addWidget(page);
         page_2 = new QWidget();
         page_2->setObjectName(QStringLiteral("page_2"));
@@ -97,10 +127,109 @@ public:
         pushButton_3->setGeometry(QRect(500, 280, 211, 91));
         pushButton_4 = new QPushButton(page_2);
         pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
-        pushButton_4->setGeometry(QRect(20, 340, 91, 31));
+        pushButton_4->setGeometry(QRect(20, 350, 91, 31));
+        label_4 = new QLabel(page_2);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(20, 10, 171, 51));
+        QFont font3;
+        font3.setFamily(QStringLiteral("Julius Sans One"));
+        font3.setPointSize(22);
+        label_4->setFont(font3);
+        label_5 = new QLabel(page_2);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(20, 60, 171, 91));
+        QFont font4;
+        font4.setFamily(QStringLiteral("Michroma"));
+        label_5->setFont(font4);
+        label_6 = new QLabel(page_2);
+        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setGeometry(QRect(240, 30, 64, 15));
+        pushButton_6 = new QPushButton(page_2);
+        pushButton_6->setObjectName(QStringLiteral("pushButton_6"));
+        pushButton_6->setGeometry(QRect(360, 70, 91, 41));
+        pushButton_7 = new QPushButton(page_2);
+        pushButton_7->setObjectName(QStringLiteral("pushButton_7"));
+        pushButton_7->setGeometry(QRect(610, 70, 91, 41));
+        pushButton_8 = new QPushButton(page_2);
+        pushButton_8->setObjectName(QStringLiteral("pushButton_8"));
+        pushButton_8->setGeometry(QRect(360, 130, 91, 41));
+        pushButton_9 = new QPushButton(page_2);
+        pushButton_9->setObjectName(QStringLiteral("pushButton_9"));
+        pushButton_9->setGeometry(QRect(610, 130, 91, 41));
+        label_7 = new QLabel(page_2);
+        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setGeometry(QRect(240, 80, 121, 16));
+        label_8 = new QLabel(page_2);
+        label_8->setObjectName(QStringLiteral("label_8"));
+        label_8->setGeometry(QRect(470, 80, 111, 31));
+        label_9 = new QLabel(page_2);
+        label_9->setObjectName(QStringLiteral("label_9"));
+        label_9->setGeometry(QRect(240, 130, 111, 21));
+        label_10 = new QLabel(page_2);
+        label_10->setObjectName(QStringLiteral("label_10"));
+        label_10->setGeometry(QRect(470, 130, 131, 31));
+        groupBox = new QGroupBox(page_2);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        groupBox->setGeometry(QRect(180, 230, 271, 141));
+        label_19 = new QLabel(groupBox);
+        label_19->setObjectName(QStringLiteral("label_19"));
+        label_19->setGeometry(QRect(80, 90, 181, 20));
+        label_16 = new QLabel(groupBox);
+        label_16->setObjectName(QStringLiteral("label_16"));
+        label_16->setGeometry(QRect(197, 50, 16, 16));
+        label_18 = new QLabel(groupBox);
+        label_18->setObjectName(QStringLiteral("label_18"));
+        label_18->setGeometry(QRect(217, 70, 41, 16));
+        label_14 = new QLabel(groupBox);
+        label_14->setObjectName(QStringLiteral("label_14"));
+        label_14->setGeometry(QRect(7, 90, 51, 16));
+        label_17 = new QLabel(groupBox);
+        label_17->setObjectName(QStringLiteral("label_17"));
+        label_17->setGeometry(QRect(217, 50, 41, 16));
+        label_15 = new QLabel(groupBox);
+        label_15->setObjectName(QStringLiteral("label_15"));
+        label_15->setGeometry(QRect(7, 110, 64, 15));
+        label_13 = new QLabel(groupBox);
+        label_13->setObjectName(QStringLiteral("label_13"));
+        label_13->setGeometry(QRect(7, 70, 81, 16));
+        label_12 = new QLabel(groupBox);
+        label_12->setObjectName(QStringLiteral("label_12"));
+        label_12->setGeometry(QRect(7, 50, 71, 16));
+        label_20 = new QLabel(groupBox);
+        label_20->setObjectName(QStringLiteral("label_20"));
+        label_20->setGeometry(QRect(170, 110, 81, 20));
         stackedWidget->addWidget(page_2);
         page_3 = new QWidget();
         page_3->setObjectName(QStringLiteral("page_3"));
+        label_11 = new QLabel(page_3);
+        label_11->setObjectName(QStringLiteral("label_11"));
+        label_11->setGeometry(QRect(20, 10, 171, 51));
+        label_11->setFont(font3);
+        label_21 = new QLabel(page_3);
+        label_21->setObjectName(QStringLiteral("label_21"));
+        label_21->setGeometry(QRect(20, 130, 391, 111));
+        label_21->setFont(font4);
+        label_22 = new QLabel(page_3);
+        label_22->setObjectName(QStringLiteral("label_22"));
+        label_22->setGeometry(QRect(20, 90, 381, 16));
+        QFont font5;
+        font5.setFamily(QStringLiteral("Nimbus Sans L"));
+        font5.setPointSize(18);
+        font5.setBold(true);
+        font5.setWeight(75);
+        label_22->setFont(font5);
+        pushButton_10 = new QPushButton(page_3);
+        pushButton_10->setObjectName(QStringLiteral("pushButton_10"));
+        pushButton_10->setGeometry(QRect(20, 350, 181, 31));
+        lineEdit = new QLineEdit(page_3);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setGeometry(QRect(460, 180, 241, 29));
+        pushButton_11 = new QPushButton(page_3);
+        pushButton_11->setObjectName(QStringLiteral("pushButton_11"));
+        pushButton_11->setGeometry(QRect(570, 230, 121, 51));
+        label_23 = new QLabel(page_3);
+        label_23->setObjectName(QStringLiteral("label_23"));
+        label_23->setGeometry(QRect(460, 150, 161, 16));
         stackedWidget->addWidget(page_3);
         ClienteTickets->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(ClienteTickets);
@@ -116,7 +245,7 @@ public:
 
         retranslateUi(ClienteTickets);
 
-        stackedWidget->setCurrentIndex(0);
+        stackedWidget->setCurrentIndex(2);
 
 
         QMetaObject::connectSlotsByName(ClienteTickets);
@@ -136,6 +265,45 @@ public:
         pushButton_5->setText(QApplication::translate("ClienteTickets", "> Descubrir", Q_NULLPTR));
         pushButton_3->setText(QApplication::translate("ClienteTickets", "\302\241Comprar!", Q_NULLPTR));
         pushButton_4->setText(QApplication::translate("ClienteTickets", "Cancelar", Q_NULLPTR));
+        label_4->setText(QApplication::translate("ClienteTickets", "UrbanEDD", Q_NULLPTR));
+        label_5->setText(QApplication::translate("ClienteTickets", "Encuentra   el   ticket\n"
+"que se adecue mejor \n"
+"a    tus     necesidades", Q_NULLPTR));
+        label_6->setText(QApplication::translate("ClienteTickets", "Precios:", Q_NULLPTR));
+        pushButton_6->setText(QApplication::translate("ClienteTickets", "Standard\n"
+"Q3.00", Q_NULLPTR));
+        pushButton_7->setText(QApplication::translate("ClienteTickets", "Premium\n"
+"Q5.00", Q_NULLPTR));
+        pushButton_8->setText(QApplication::translate("ClienteTickets", "Full\n"
+"Q50.00", Q_NULLPTR));
+        pushButton_9->setText(QApplication::translate("ClienteTickets", "Endless\n"
+"Q100.00", Q_NULLPTR));
+        label_7->setText(QApplication::translate("ClienteTickets", "\302\277Un viaje corto?", Q_NULLPTR));
+        label_8->setText(QApplication::translate("ClienteTickets", "Ida y vuelta\n"
+"(Ahorra un 10%)", Q_NULLPTR));
+        label_9->setText(QApplication::translate("ClienteTickets", "\302\277Vas para largo?", Q_NULLPTR));
+        label_10->setText(QApplication::translate("ClienteTickets", "\302\241El viaje de tu vida!\n"
+"(Mejor opci\303\263n)", Q_NULLPTR));
+        groupBox->setTitle(QApplication::translate("ClienteTickets", "Cuenta", Q_NULLPTR));
+        label_19->setText(QApplication::translate("ClienteTickets", "Fecha", Q_NULLPTR));
+        label_16->setText(QApplication::translate("ClienteTickets", "#", Q_NULLPTR));
+        label_18->setText(QApplication::translate("ClienteTickets", "AAAA", Q_NULLPTR));
+        label_14->setText(QApplication::translate("ClienteTickets", "Fecha:", Q_NULLPTR));
+        label_17->setText(QApplication::translate("ClienteTickets", "0000", Q_NULLPTR));
+        label_15->setText(QApplication::translate("ClienteTickets", "Total:", Q_NULLPTR));
+        label_13->setText(QApplication::translate("ClienteTickets", "Verificacion:", Q_NULLPTR));
+        label_12->setText(QApplication::translate("ClienteTickets", "Tu c\303\263digo:", Q_NULLPTR));
+        label_20->setText(QApplication::translate("ClienteTickets", "Por calcular", Q_NULLPTR));
+        label_11->setText(QApplication::translate("ClienteTickets", "UrbanEDD", Q_NULLPTR));
+        label_21->setText(QApplication::translate("ClienteTickets", "Sentimos profundamente que desees\n"
+"reembolsar tu ticket. Si en algun momento\n"
+"haz sentido la menor insatisfaccion, no dudes\n"
+"en enviarnos tu queja o sugerencia a\n"
+" urbanedd@support.com", Q_NULLPTR));
+        label_22->setText(QApplication::translate("ClienteTickets", "A veces, no tenemos a donde ir...", Q_NULLPTR));
+        pushButton_10->setText(QApplication::translate("ClienteTickets", "Cancelar (Lo sabiamos)", Q_NULLPTR));
+        pushButton_11->setText(QApplication::translate("ClienteTickets", "Reembolsar", Q_NULLPTR));
+        label_23->setText(QApplication::translate("ClienteTickets", "Codigo de verificacion:", Q_NULLPTR));
     } // retranslateUi
 
 };
