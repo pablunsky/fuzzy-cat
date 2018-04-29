bb47b7559cf536aad5c8305a8081fee55ff68796
pablunsky <hpablo677@gmail.com>
2018-04-29 00:38:39 -0600

    ArbolB anadido

---
+++ Server/src/main/java/Estructuras/ArbolB.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estructuras;
+
+/**
+ *
+ * @author pablunsky
+ */
+public class ArbolB {
+    private int orden;
+    private int tamano;
+    private NodoArbolB raiz;    
+
+    public int getOrden() {
+        return orden;
+    }
+
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public NodoArbolB getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoArbolB raiz) {
+        this.raiz = raiz;
+    }
+}
