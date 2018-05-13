f7e435bff68666b051d21036028ad15bc416746d
pablunsky <hpablo677@gmail.com>
2018-05-12 21:41:18 -0600

    Clases modificadas

--- servidor/src/main/java/edd/urban/servidor/ArbolB.java
+++ servidor/src/main/java/edd/urban/servidor/ArbolB.java
@@ -10,5 +10,54 @@ package edd.urban.servidor;
  * @author pablunsky
  */
 public class ArbolB {
+    private NodoArbolB raiz;
+    private int orden;
+    private int tamano;
+
+    public ArbolB(int orden, int tamano){
+        this.orden = orden;
+        this.tamano = tamano;
+        this.raiz = null;
+    }
+    
+    public void AddNodo(Object valor){
+        if(raiz == null){
+            raiz = new NodoArbolB(orden);
+            raiz.AddValor(valor);
+            return;
+        }
+        NodoArbolB aux = raiz;
+        if(aux.AddValor(valor))
+            return;
+        
+        
+    
+    }
+    
+    public NodoArbolB getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoArbolB raiz) {
+        this.raiz = raiz;
+    }
+
+    public int getOrden() {
+        return orden;
+    }
+
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+    
+    
     
 }
